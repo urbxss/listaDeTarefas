e2502f129f517c93ee0f6159a1585fac9e5041ea
jesseca <jessicameiradeoliveira@gmail.com>
2025-12-20 18:57:50 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function adicionarTarefa() {
         mensagem.textContent = mensagemSucesso
         tarefas.push(tarefa)
         renderizarTarefas()
-
     }
 
     inputTarefa.value = ""
@@ -25,10 +24,42 @@ function renderizarTarefas() {
     const listaTarefas = document.getElementById("listaTarefas")
     listaTarefas.innerHTML = ""
 
-    let i = 0
-    for (i; i < tarefas.length; i++){
+    for (let i = 0; i < tarefas.length; i++) {
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas[i]
+
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+        
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
         listaTarefas.appendChild(novaTarefa)
     }
+}
+
+function removerTarefa(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}   
+
+function editarTarefa(i) {
+    let tarefaEditada = prompt("Edite a tarefa:")
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    }
+}
+
+function limparLista() {
+    tarefas.length = 0
+    renderizarTarefas()
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent = "Lista de tarefas limpa com sucesso!"
 }
