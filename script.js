a502623939fa1cc3654285199016d337060bb543
jesseca <jessicameiradeoliveira@gmail.com>
2025-12-20 18:34:29 -0300

    guardando em arrays

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
+let tarefas = []
+
 function adicionarTarefa() {
 
-    //recebe valor do input do usuário
     const inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value.trim()
 
     const mensagem = document.getElementById("mensagem")
 
-    // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
     if (tarefa == "") {
-        //mostre uma mensagem de erro
         let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
         mensagem.textContent = mensagemErro
     } else {
-        //mensagem de tarefa adicionada com sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
         mensagem.textContent = mensagemSucesso
+        tarefas.push(tarefa)
+        renderizarTarefas()
 
-        //cria novo item (li) e insere na (lista ul)
-        const listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
-        novaTarefa.textContent = tarefa
-        listaTarefas.appendChild(novaTarefa)
     }
 
-    //limpa o input do usuário
     inputTarefa.value = ""
+}
+
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML = ""
+
+    let i = 0
+    for (i; i < tarefas.length; i++){
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefas[i]
+        listaTarefas.appendChild(novaTarefa)
+    }
 }
