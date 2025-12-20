8b88f9df5e7e0c5845b28ae4a39741ad54a8aa12
jesseca <jessicameiradeoliveira@gmail.com>
2025-12-20 17:45:43 -0300

    tratamento de erros feito

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-function adicionarTarefa(){
+function adicionarTarefa() {
 
-    // Pega o id do input
-    let inputTarefas = document.getElementById("inputTarefas")
-    // Atribui o valor colocado no input para tarefas
-    let tarefa = inputTarefas.value
-    // Pega o id da ul no html
-    let listaTarefas = document.getElementById("listaTarefas")
-    // Cria uma lista na variável novaTarefa
-    let novaTarefa = document.createElement("li")
-    // Pega o conteúdo de tarefa e coloca em novaTarefa
-    novaTarefa.textContent = tarefa
-    // Coloca novaTarefa como filha de listaTarefas
-    listaTarefas.appendChild(novaTarefa)
-    // Cria uma mensagem
-    let mensagem = "Sua tarefa foi adicionada com sucesso!"
-    // Pega o id mensagem no html e coloca a mensagem de cima
-    document.getElementById("mensagem").textContent = mensagem
-    // Limpa o campo do input toda vez que uma tarefa é adicionada
-    inputTarefas.value=""
-    
+    //recebe valor do input do usuário
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+
+    const mensagem = document.getElementById("mensagem")
+
+    // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
+    if (tarefa == "") {
+        //mostre uma mensagem de erro
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
+    } else {
+        //mensagem de tarefa adicionada com sucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+
+        //cria novo item (li) e insere na (lista ul)
+        const listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        listaTarefas.appendChild(novaTarefa)
+    }
+
+    //limpa o input do usuário
+    inputTarefa.value = ""
 }
